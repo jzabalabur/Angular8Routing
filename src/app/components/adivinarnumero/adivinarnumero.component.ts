b003cd4a7015a33fd75bdb1f7a6ce40c0bb162f5
Jon Zabala <j_zabalabur@fpzornotza.com>
2024-12-19 10:37:03 +0100

    Ariketak

--- src/app/components/adivinarnumero/adivinarnumero.component.ts
+++ src/app/components/adivinarnumero/adivinarnumero.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-adivinarnumero',
   templateUrl: './adivinarnumero.component.html',
   styleUrls: ['./adivinarnumero.component.scss']
 })
 export class AdivinarnumeroComponent {
+  numero:Number=0;
+  adivinar:Boolean=false;
+  numeroI = new FormControl(0);
 
+  generarNumero(){
+    Math.floor(Math.random() * 50) + 1,
+    this.adivinar=true;
+  }
+  comprobar(){
+    //??????????????
+    console.log(this.numeroI);
+  }
+  exito(){
+
+  }
+  fracaso(){
+
+  }
 }
